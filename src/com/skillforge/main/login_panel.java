c4932f7a92bdc33ddef85f1062008f3a44c82360
MohamedWael18 <mohamedwael182004@gmail.com>
2025-11-21 23:33:16 +0200

    finished the GUI and added the logout feature and some other changes

---
+++ src/com/skillforge/main/login_panel.java
@@ -0,0 +1,160 @@
+package com.skillforge.main;
+
+import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import com.google.gson.JsonObject;
+import com.skillforge.db.CoursesDatabaseManager;
+import com.skillforge.db.UserDatabaseManager;
+import org.json.JSONArray;
+import org.json.JSONObject;
+import java.io.File;
+import java.nio.file.Files;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import com.skillforge.Utilities.*;
+import com.skillforge.model.*;
+
+public class login_panel extends JFrame{
+    private JPanel container2;
+    private JTextField emailfield;
+    private JPasswordField passwordField1;
+    private JButton loginButton;
+    private JComboBox comboBox1;
+    private JButton backButton;
+
+    public login_panel() {
+        setTitle("Login");
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setSize(400, 300);
+        setLocationRelativeTo(null);
+        setContentPane(container2);
+
+        loginButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent actionEvent) {
+                login_operations();
+            }
+        });
+        backButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent actionEvent) {
+                goback();
+                dispose();
+            }
+        });
+    }
+
+    private void login_operations(){
+            String type = (String) comboBox1.getSelectedItem();
+            String email = emailfield.getText().trim();
+            String password = new String(passwordField1.getPassword());
+
+            File file = new File("users.json");
+            if (!file.exists()) {
+                System.out.println("ERROR: users.json file does not exist.");
+                return;
+            }
+            try {
+                Student S = null;
+                Instructor I = null;
+                String content = new String(Files.readAllBytes(file.toPath()));
+                if (content.trim().isEmpty()) {
+                    JOptionPane.showMessageDialog(this, "No users found!", "Login Error", JOptionPane.ERROR_MESSAGE);
+                    return;
+                }
+                JSONArray usersArray = new JSONArray(content);
+                boolean found = false;
+                for (int i = 0; i < usersArray.length(); i++) {
+                    JSONObject userObj = usersArray.getJSONObject(i);
+                    String userID = userObj.getString("userID");
+                    String username = userObj.getString("userName");
+                    String userEmail = userObj.getString("email");
+                    String userRole = userObj.getString("role");
+                    String userPasswordHash = userObj.getString("passwordHash");
+                    JSONArray EnrolledCoursesArray;
+                    JSONArray CreatedcoursesArray;
+                    JSONObject progressObj;
+
+                    if (userEmail.equals(email) && userRole.equals(type) && userPasswordHash.equals(securityUtils.hashPassword(password))) {
+                        found = true;
+                        if(userRole.equals("Student")){
+                            EnrolledCoursesArray = userObj.getJSONArray("enrolledCourses");
+                            progressObj = userObj.getJSONObject("progress");
+                            List<String> EnrolledCourses = getCoursesList(EnrolledCoursesArray);
+                            Map<String, List<String>> progress = getProgressMap(progressObj);
+                            S = new Student(userID,userRole,username,userEmail,userPasswordHash,EnrolledCourses,progress);
+                        }
+                        else if(userRole.equals("Instructor")){
+                            CreatedcoursesArray = userObj.getJSONArray("createdCourses");
+                            List<String> CreatedCourses = getCoursesList(CreatedcoursesArray);
+                            I = new Instructor(userID,"Instructor",username,userEmail,userPasswordHash,CreatedCourses);
+                        }
+                        break;
+                    }
+                }
+                if (found) {
+                    JOptionPane.showMessageDialog(this, "Login successful!", "Success", JOptionPane.INFORMATION_MESSAGE);
+                    emailfield.setText("");
+                    passwordField1.setText("");
+                    if(type.equals("Student")) openStudentDashboard(S);
+                    else if(type.equals("Instructor")) openInstructorDashborad(I);
+                    dispose();
+                } else {
+                    JOptionPane.showMessageDialog(this, "Invalid credentials or user type!", "Login Error", JOptionPane.ERROR_MESSAGE);
+                }
+            } catch (Exception e) {
+                System.out.println("ERROR: Failed to read users.json");
+            }
+        }
+
+    private List<String> getCoursesList(JSONArray array){
+        List<String> Courses = new ArrayList<>();
+        for (int j = 0; j < array.length(); j++) {
+            Courses.add(array.getString(j));
+        }
+        return Courses;
+    }
+
+    private Map<String, List<String>> getProgressMap(JSONObject object){
+        Map<String, List<String>> progress = new HashMap<>();
+        for (String course : object.keySet()) {
+            JSONArray lessonsArray = object.getJSONArray(course);
+            List<String> lessons = new ArrayList<>();
+            for (int j = 0; j < lessonsArray.length(); j++) {
+                lessons.add(lessonsArray.getString(j));
+            }
+            progress.put(course, lessons);
+        }
+        return progress;
+    }
+
+    private void goback() {
+
+        SwingUtilities.invokeLater(() -> {
+            first_panel back = new first_panel();
+            back.setVisible(true);
+        });
+    }
+
+    private void openStudentDashboard(Student s){
+        UserDatabaseManager us=new UserDatabaseManager("users.json");
+        CoursesDatabaseManager cs=new CoursesDatabaseManager();
+        SwingUtilities.invokeLater(() -> {
+            StudentDashboard panel = new StudentDashboard(s,cs,us);
+            panel.setVisible(true);
+        });
+    }
+    private void openInstructorDashborad(Instructor i){
+        UserDatabaseManager us=new UserDatabaseManager("users.json");
+        CoursesDatabaseManager cs=new CoursesDatabaseManager();
+        SwingUtilities.invokeLater(() -> {
+            InstructorDashboardFrame panel = new InstructorDashboardFrame(i,cs,us);
+            panel.setVisible(true);
+        });
+    }
+}
---
+++ src/com/skillforge/main/signup_panel.java
@@ -0,0 +1,187 @@
+package com.skillforge.main;
+
+import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.HashSet;
+import java.util.Random;
+import java.util.Set;
+
+import com.skillforge.model.*;
+import com.skillforge.Utilities.*;
+import com.skillforge.db.*;
+import org.json.JSONArray;
+import org.json.JSONObject;
+
+public class signup_panel extends JFrame {
+    private JPanel container3;
+    private JComboBox comboBox1;
+    private JTextField Emailfield;
+    private JPasswordField passwordField1;
+    private JPasswordField passwordField2;
+    private JButton signupButton;
+    private JTextField namefield;
+    private JButton backButton;
+
+    public signup_panel() {
+        setTitle("Signup");
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setSize(400, 300);
+        setLocationRelativeTo(null);
+        setContentPane(container3);
+
+        signupButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent actionEvent) {
+                Signup_operations();
+            }
+        });
+        backButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent actionEvent) {
+                goback();
+                dispose();
+            }
+        });
+    }
+
+    private void Signup_operations() {
+        String Usertype = (String) comboBox1.getSelectedItem();
+        String Username = namefield.getText().trim();
+        String Email = Emailfield.getText().trim();
+        String password1 = new String(passwordField1.getPassword());
+        String password2 = new String(passwordField2.getPassword());
+
+        if (Usertype.isEmpty() || Username.isEmpty() || Email.isEmpty() || password1.isEmpty() || password2.isEmpty())
+            JOptionPane.showMessageDialog(this, "All fields must be filled!", "Input Error", JOptionPane.ERROR_MESSAGE);
+
+        else if (!ValidName(Username))
+            JOptionPane.showMessageDialog(this, "Invalid Username", "Input Error", JOptionPane.ERROR_MESSAGE);
+
+        else if (!ValidEmail(Email))
+            JOptionPane.showMessageDialog(this, "Invalid Email Format", "Input Error", JOptionPane.ERROR_MESSAGE);
+
+        else if (!ValidPassword(password1))
+            JOptionPane.showMessageDialog(this, "Invalid Password", "Input Error", JOptionPane.ERROR_MESSAGE);
+
+        else if (!password1.equals(password2))
+            JOptionPane.showMessageDialog(this, "Passwords don't match", "Input Error", JOptionPane.ERROR_MESSAGE);
+        else {
+            String ID = IDgenerator();
+            if (Usertype.equals("Student")) {
+                User newUser = new Student(
+                        ID,
+                        "Student",
+                        Username,
+                        Email,
+                        securityUtils.hashPassword(password1),
+                        null,
+                        null
+                );
+                UserDatabaseManager db = new UserDatabaseManager("users.json");
+                db.add(newUser);
+                db.saveData();
+                JOptionPane.showMessageDialog(this, "Successful signup", "Success", JOptionPane.INFORMATION_MESSAGE);
+                namefield.setText("");
+                passwordField1.setText("");
+                passwordField2.setText("");
+                Emailfield.setText("");
+                openloginpanel();
+                dispose();
+            } else if (Usertype.equals("Instructor")) {
+                User newUser = new Instructor(
+                        ID,
+                        "Instructor",
+                        Username,
+                        Email,
+                        securityUtils.hashPassword(password1),
+                        null
+                );
+                UserDatabaseManager db = new UserDatabaseManager("users.json");
+                db.add(newUser);
+                db.saveData();
+                JOptionPane.showMessageDialog(this, "Successful signup", "Success", JOptionPane.INFORMATION_MESSAGE);
+                namefield.setText("");
+                passwordField1.setText("");
+                passwordField2.setText("");
+                Emailfield.setText("");
+                openloginpanel();
+                dispose();
+            }
+        }
+    }
+
+    private boolean ValidName(String name) {
+        if (name.matches("[a-zA-Z]+") && name.length() <= 15) return true;
+        else return false;
+    }
+
+    private boolean ValidEmail(String email) {
+        if (email == null || email.isEmpty()) {
+            return false;
+        }
+        String emailRegex = "^[A-Za-z0-9+_.-]+@[A-Za-z0-9.-]+\\.com$";
+        return email.matches(emailRegex);
+    }
+
+    private boolean ValidPassword(String password) {
+        if (password == null || password.length() < 8) {
+            return false;
+        }
+        boolean hasLetter = false;
+        boolean hasNonLetter = false;
+        for (char c : password.toCharArray()) {
+            if (Character.isLetter(c)) {
+                hasLetter = true;
+            } else {
+                hasNonLetter = true;
+            }
+            if (hasLetter && hasNonLetter) {
+                return true;
+            }
+        }
+        return hasLetter && hasNonLetter;
+    }
+
+    private String IDgenerator() {
+        try {
+            String content = new String(Files.readAllBytes(Paths.get("users.json")));
+            JSONArray usersArray = new JSONArray(content);
+            Set<String> existingIDs = new HashSet<>();
+            for (int i = 0; i < usersArray.length(); i++) {
+                JSONObject userObj = usersArray.getJSONObject(i);
+                existingIDs.add(userObj.getString("userID"));
+            }
+            Random random = new Random();
+            String newID;
+            do {
+                int number = random.nextInt(9000) + 1000; // ensures 1000–9999
+                newID = String.valueOf(number);
+            } while (existingIDs.contains(newID));
+
+            return newID;
+
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    private void goback() {
+
+        SwingUtilities.invokeLater(() -> {
+            first_panel back = new first_panel();
+            back.setVisible(true);
+        });
+    }
+
+    private void openloginpanel() {
+
+        SwingUtilities.invokeLater(() -> {
+            login_panel login = new login_panel();
+            login.setVisible(true);
+        });
+    }
+}
