5e377ad6714e203cbeb4069ea1528821b2f62ea0
YehiaGazzarine <yahya.gazzarine.mg@gmail.com>
2025-11-23 01:57:42 +0200

    2

---
+++ src/com/skillforge/main/CourseApprovalService.java
@@ -0,0 +1,26 @@
+package com.skillforge.main;
+
+import com.skillforge.db.CoursesDatabaseManager;
+import com.skillforge.model.Course;
+
+import java.util.List;
+
+public class CourseApprovalService {
+    private final CoursesDatabaseManager courseDB;
+
+    public CourseApprovalService(CoursesDatabaseManager db) {
+        this.courseDB = db;
+    }
+
+    public List<Course> getPendingCourses() {
+        return courseDB.getPendingCourses();
+    }
+
+    public void approveCourse(String courseId) {
+        courseDB.updateCourseStatus(courseId, "APPROVED");
+    }
+
+    public void rejectCourse(String courseId) {
+        courseDB.updateCourseStatus(courseId, "REJECTED");
+    }
+}
---
+++ src/com/skillforge/model/Admin.java
@@ -0,0 +1,22 @@
+package com.skillforge.model;
+
+public class Admin extends User {
+
+    public Admin(String userId, String username, String email, String passwordHash) {
+        setUserID(userId);
+        setUserName(username);
+        setRole("ADMIN");
+        setEmail(email);
+        setPasswordHash(passwordHash);
+    }
+
+    @Override
+    public String getID() {
+        return getUserID();
+    }
+
+    @Override
+    public String toString() {
+        return getID() + " " + getUserName() + " " + getRole() + " " + getEmail();
+    }
+}
